7dc69f5af22fd52e5dfcd0c0e0940f6a08827ef9
arkduni <arkduni@gmail.com>
2023-03-19 20:24:55 +0900

    장바구니 아이콘

--- CodeGreen/src/main/java/com/greener/codegreen/controller/PaymentController.java
+++ CodeGreen/src/main/java/com/greener/codegreen/controller/PaymentController.java
@@ -2,6 +2,10 @@ package com.greener.codegreen.controller;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -54,6 +59,18 @@ public class PaymentController {
 			res = "toPayment";
 		}
 		return res;
+	}
+	/*
+	 * 장바구니 개수 반환
+	 */
+
+	@RequestMapping(value = "/cartcount", method = RequestMethod.POST)
+	@CrossOrigin(origins="http://localhost:8080")
+	public @ResponseBody String CartCount(@RequestBody CartDTO cartDTO) throws Exception {
+		String res = "";
+		int cartCount = paymentService.getCartCount(cartDTO);
+
+		return Integer.toString(cartCount);
 	}
 	
 	/*
